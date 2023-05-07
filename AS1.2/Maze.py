6536d85169d3d5ba6de06be1800020af9b68d703
BKBetz <brandonbetz@live.nl>
2023-05-07 18:53:48 +0200

    finished 1.2

--- AS1.2/Maze.py
+++ AS1.2/Maze.py
@@ -1,31 +1,45 @@
 """Maze the agents walks in"""
 
 import numpy as np
-from typing import List
-import random as rd
-
 
 class Maze:
-    """The Maze"""
-    def __init__(self, maze_format: List, reward: int):
-        self.maze = maze_format
-        self.reward = reward
+    """The Maze, contains functions for finding bordering and updating states, making a step and creating a new maze"""
+    def __init__(self):
+        self.states = ""
         self.actions = {0: [-1, 0], 1: [1, 0], 2: [0, -1], 3: [0, 1]}
+
+    def get_states(self, state):
+        """
+            Return the bordering states of a state
+        :return:
+            state: a given state
+        """
+        states = {}
+        for action in self.actions.keys():
+            # for each action, fill the states dict with the coordinate as key and list as value
+            states[self.step(state, action)] = self.states[self.step(state, action)]
+
+        return states
 
     def create_maze(self):
         """
             Create a maze using the maze_format given at the start. Turns each value of that maze into a state with
             coordinates and rewards
         """
-        matrix = np.zeros((self.maze[0], self.maze[1]))
-        self.maze = self.create_states(matrix)
+        matrix = np.zeros((4, 4))
+        rewards = np.array([[-1, -1, -1, 40],
+                  [-1, -1, -10, -10],
+                  [-1, -1, -1, -1],
+                  [10, -2, -1, -1]])
+        self.states = self.create_states(matrix, rewards)
 
-    def create_states(self, maze: np.array):
+    def create_states(self, maze: np.array, rewards: np.array):
         """
-        Gives each value in a maze: coordinates, a reward and a boolean for terminal state.
-        At the end, two states get randomly picked to be terminal states
+        Gives each cell in a maze: coordinates, a reward, a value and a boolean for terminal state.
+        At the end, two states get picked to be terminal states
         :param
             maze: The empty maze
+            rewards: the rewards for each state
         :return:
             states: The new maze with states
         """
@@ -33,16 +47,15 @@ class Maze:
         # easy way to get all coordinates using numpy
         for x, y in np.ndindex(maze.shape):
             coord = (x, y)
-            states[coord] = [self.reward, False]
+            states[coord] = [rewards[x][y], 0, False]
 
-        # determine terminal states
-        terminal_states = rd.choices(list(states), k=2)
-        for state in terminal_states:
-            states[state][1] = True
+        # set terminal states
+        states[(0, 3)][2] = True
+        states[(3, 0)][2] = True
 
         return states
 
-    def step(self, c_state: list, action: list):
+    def step(self, c_state: tuple, action: int):
         """
         Determine the new position given a state and a action
         :param
@@ -52,10 +65,20 @@ class Maze:
             new_pos: The new coordinates if it is possible (within the maze)
             c_state: The current state if the new position is not possible (not within the maze)
         """
-
         # new pos = sum from current state plus the step you make with a action
         new_pos = tuple(sum(x) for x in zip(c_state, self.actions[action]))
-        if new_pos in self.maze.keys():
+        if new_pos in self.states.keys():
             return new_pos
         else:
             return c_state
+
+    def update_states(self, new_values):
+        """
+            Updates old values with new values after a iteration
+
+            :param
+                new_values: new values after a value iteration
+        """
+
+        for coords, value in new_values.items():
+            self.states[coords][1] = value
---
+++ AS1.2/Policy.py
@@ -0,0 +1,69 @@
+import random as rd
+
+
+class Policy:
+    """
+        The policy of the agent. contains the value function and a action function
+    """
+    def __init__(self):
+        """
+        -1 means that there is no better action. Meaning the either:
+        1: No states are better than the state that's being used
+        2: Reached a terminal state
+        """
+        self.actions = {0: [-1, 0], 1: [1, 0], 2: [0, -1], 3: [0, 1], -1: [0, 0]}
+
+    def value_func(self, next_states, discount):
+        """
+        Calculate the value given the states bordering current state
+
+        :param
+            next_states: The states bordering the current states
+            discount: The discountfactor
+        :return:
+        """
+
+        values = []
+        for coord, items in next_states.items():
+            sum = items[0] + discount * items[1]
+            values.append(sum)
+
+        new_value = max(values)
+
+        return new_value
+
+    def select_random_action(self):
+        """
+        Select a random action
+        :return:
+            a random choice for an action
+        """
+        return rd.choice([0, 1, 2, 3])
+
+    def decide_action(self, state, next_states):
+        """
+        A function for finding the best action using the current position and
+        finding the bordering state with the highest utility.
+        :param
+            state: current state
+            next_states: bordering states
+        :return:
+            an action
+        """
+        # find the state with the highest utility
+        best_state = max(next_states, key=next_states.get)
+        move = []
+        for i in range(0, len(state)):
+            # the move that needs to be made to go from the current state tot the best state
+            move.append(best_state[i] - state[i])
+
+        # the key equal to the move that needs to be made
+        action = [k for k, v in self.actions.items() if v == move]
+        return action[0]
+
+
+
+
+
+
+
